f3aa9f9b7e1ffdba5e04858e4e20f2798a1ff581
BRIANNGETICH12 <briangetich95@gmail.com>
2022-12-13 07:59:36 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 /*
- * File: 0-positive_or_negative.c
- *  Auth: Brennan D Baraban
- *  */
+ * * File: 0-positive_or_negative.c
+ * Auth: brian ngetich
+ */
 
 #include <stdlib.h>
 #include <time.h>
@@ -9,7 +9,7 @@
 
 /**
  * main - Prints a random number and states whether
- * it is positive, negative, or zero.
+ *        it is positive, negative, or zero.
  *
  * Return: Always 0.
  */
@@ -22,7 +22,7 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0)
+	else if(n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
